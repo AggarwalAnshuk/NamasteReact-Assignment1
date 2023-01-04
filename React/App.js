c4c7d5f9335738f0a8dfdcfdfa4d29a138421285
Anshuk Aggarwal <aggar@LAPTOP-OSKI3RTS>
2023-01-04 16:04:22 -0500

    added nested react elements

--- React/App.js
+++ React/App.js
@@ -1,7 +1,14 @@
-const heading = React.createElement("h1", {}, "Hello World-React");
+const heading1 = React.createElement("h1", {}, "Hello World-React h1");
+const heading2 = React.createElement("h2", {}, "Hello World-React h2");
+
+const container = React.createElement(
+  "section",
+  { id: "container", className: "container" },
+  [heading1, heading2]
+);
 
 const rootEl = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootEl);
 
-root.render(heading);
+root.render(container);
