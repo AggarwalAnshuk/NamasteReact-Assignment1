7a525884b9f0683c3b10467fe5506845ed98fe94
Anshuk Aggarwal <aggar@LAPTOP-OSKI3RTS>
2023-01-04 15:57:07 -0500

    add React

---
+++ React/App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {}, "Hello World-React");
+
+const rootEl = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootEl);
+
+root.render(heading);
